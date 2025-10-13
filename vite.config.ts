92bbce02c5218cd180539c98367106559cc2418e
enjoykanyu <creatkanyu@outlook.com>
2025-10-13 23:34:17 +0800

    请求

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   css: {
     preprocessorOptions: {
